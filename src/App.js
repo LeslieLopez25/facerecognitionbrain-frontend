4047a7fa33fd1b3c25a8fd2a7dd6ec25546c3a6d
Leslie Lopez <leslie2515lopez@gmail.com>
2022-11-24 21:19:51 -0600

    Changed particles

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ class App extends Component {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <ParticlesBg type="lines" bg={true} />
+        <ParticlesBg type="ball" bg={true} />
         <Navigation
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}
