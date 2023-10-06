5087c3cb921990b472e25962c58380d030d193c2
Leslie Lopez <leslie2515lopez@gmail.com>
2023-10-05 23:28:54 -0600

    Updated FaceRecognition.js

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,29 +3,21 @@ import "./FaceRecognition.css";
 
 const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
-    <div className="center ma">
-      <div className="absolute mt2">
-        <img
-          id="inputimage"
-          alt=""
-          src={imageUrl}
-          width="500px"
-          height="auto"
-        />
-        {boxes.map((box) => {
-          return (
-            <div
-              key={box.topRow}
-              className="bounding-box"
-              style={{
-                top: box.topRow,
-                right: box.rightCol,
-                bottom: box.bottomRow,
-                left: box.leftCol,
-              }}
-            ></div>
-          );
-        })}
+    <div className="flex justify-center sm:w-full md:w-1/2 h-auto m-auto my-4">
+      <div className="absolute mt2 shadow-5 ma3" style={{ maxWidth: 800 }}>
+        <img alt="" src={imageUrl} id="inputimage" />
+        {boxes.map((box) => (
+          <div
+            key={box.id}
+            className="bounding-box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}
+          ></div>
+        ))}
       </div>
     </div>
   );
