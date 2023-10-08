02583fb42735ebb88df2f9167329a2ef8acc4b2a
Leslie Lopez <leslie2515lopez@gmail.com>
2023-10-07 23:51:06 -0600

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -43,18 +43,17 @@ class App extends Component {
   };
 
   calculateFaceLocation = (data) => {
+    const clarifaiFace = JSON.parse(data, null, 2).outputs[0].data.regions[0]
+      .region_info.bounding_box;
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
-    return JSON.parse(data, null, 2).outputs[0].data.regions.map((face) => {
-      const clarifaiFace = face.region_info.bounding_box;
-      return {
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - clarifaiFace.right_col * width,
-        bottomRow: height - clarifaiFace.bottom_row * height,
-      };
-    });
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
   };
 
   displayFaceBoxes = (boxes) => {
@@ -66,32 +65,49 @@ class App extends Component {
   };
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: "c833fb8738c54b64aef624046587f201",
+        app_id: "pp9glgunpdb5",
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: this.state.input,
+            },
+          },
+        },
+      ],
+    });
 
-    fetch("https://facerecognitionbrain-api-ral3.onrender.com/imageurl", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        input: this.state.input,
-      }),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        this.displayFaceBox(this.calculateFaceLocation(result));
-        if (result) {
-          fetch("https://facerecognitionbrain-api-ral3.onrender.com/image", {
-            method: "PUT",
+    fetch(
+      "https://api.clarifai.com/v2/models/f76196b43bbd45c99b4f3cd8e8b40a8a/outputs",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: process.env.API_CLARIFAI,
+        },
+        body: raw,
+      }
+    )
+      .then((response) => response.text())
+      .then((response) => {
+        if (response) {
+          fetch("https://facerecognitionbrain-api-ral3.onrender.com", {
+            method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
               id: this.state.user.id,
             }),
           })
-            .then((result) => result.json())
+            .then((response) => response.json())
             .then((count) => {
               this.setState(Object.assign(this.state.user, { entries: count }));
             });
         }
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch((error) => console.log("error", error));
   };
