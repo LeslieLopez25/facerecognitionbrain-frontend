49967badb60a282ee41e37bb5fe153528f207cf8
Leslie Lopez <leslie2515lopez@gmail.com>
2023-10-11 16:59:58 -0600

    Updated the css

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,12 @@ import React from "react";
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
-      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <nav
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
         <p
           onClick={() => onRouteChange("signout")}
           className="f3 link dim white underline pa3 pointer"
