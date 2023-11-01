57491c663649c3e16c87a06aa7e120181f7c0d99
Leslie Lopez <leslie2515lopez@gmail.com>
2023-10-31 19:42:47 -0600

    Added dropdown menu to profile component

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,21 +1,43 @@
 import React from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
 class ProfileIcon extends React.Component {
   constructor(props) {
     super(props);
+    this.toggle = this.toggle.bind(this);
     this.state = {
       dropdownOpen: false,
     };
   }
 
+  toggle = () => {
+    this.setState((prevState) => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  };
+
   render() {
     return (
-      <div class="pa4 tc">
-        <img
-          src="https://tachyons.io/img/logo.jpg"
-          class="br-100 ba h3 w3 dib"
-          alt="avatar"
-        />
+      <div className="pa4 tc">
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle caret>Dropdown</DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem>Sign Out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <div class="pa4 tc">
+          <img
+            src="https://tachyons.io/img/logo.jpg"
+            class="br-100 ba h3 w3 dib"
+            alt="avatar"
+          />
+        </div>
       </div>
     );
   }
