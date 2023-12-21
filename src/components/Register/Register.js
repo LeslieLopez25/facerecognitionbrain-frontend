3a8a469cbe601c172ae51dece362b57194460d1a
Leslie Lopez <leslie2515lopez@gmail.com>
2023-12-20 22:31:36 -0600

    Updated register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -8,7 +8,7 @@ class Register extends React.Component {
       email: "",
       password: "",
       name: "",
-      // loading: false,
+      loading: false,
     };
   }
 
@@ -25,7 +25,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    // this.setState({ loading: true });
+    this.setState({ loading: true });
     fetch("https://facerecognitionbrain-api-ral3.onrender.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
@@ -41,78 +41,78 @@ class Register extends React.Component {
           this.props.loadUser(user);
           this.props.onRouteChange("home");
         }
-        // })
-        // .catch(console.log)
-        // .finally(() => {
-        //   this.setState({ loading: false });
+      })
+      .catch(console.log)
+      .finally(() => {
+        this.setState({ loading: false });
       });
   };
 
   render() {
     return (
-      // <LoadingScreen
-      //   loading={this.state.loading}
-      //   bgColor="transparent"
-      //   spinnerColor="#ffffff"
-      //   textColor="#ffffff"
-      //   logoSrc=""
-      //   text="Loading..."
-      //   className="text-xl w-fit mx-auto backdrop-blur-sm"
-      // >
-      <article className="br3 ba b--black-40 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 white">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0 center">Register</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">
-                  Name
-                </label>
+      <LoadingScreen
+        loading={this.state.loading}
+        bgColor="transparent"
+        spinnerColor="#ffffff"
+        textColor="#ffffff"
+        logoSrc=""
+        text="Loading..."
+        className="text-xl w-fit mx-auto backdrop-blur-sm"
+      >
+        <article className="br3 ba b--black-40 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+          <main className="pa4 white">
+            <div className="measure">
+              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                <legend className="f1 fw6 ph0 mh0 center">Register</legend>
+                <div className="mt3">
+                  <label className="db fw6 lh-copy f6" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="text"
+                    name="name"
+                    id="name"
+                    onChange={this.onNameChange}
+                  />
+                </div>
+                <div className="mt3">
+                  <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                    Email
+                  </label>
+                  <input
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="email"
+                    name="email-address"
+                    id="email-address"
+                    onChange={this.onEmailChange}
+                  />
+                </div>
+                <div className="mv3">
+                  <label className="db fw6 lh-copy f6" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={this.onPasswordChange}
+                  />
+                </div>
+              </fieldset>
+              <div className="">
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="text"
-                  name="name"
-                  id="name"
-                  onChange={this.onNameChange}
+                  onClick={this.onSubmitSignIn}
+                  className="b ph3 pv2 input-reset ba b--black white bg-transparent grow pointer f6 dib"
+                  type="submit"
+                  value="Register"
                 />
               </div>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                  Email
-                </label>
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  onChange={this.onEmailChange}
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={this.onPasswordChange}
-                />
-              </div>
-            </fieldset>
-            <div className="">
-              <input
-                onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black white bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Register"
-              />
             </div>
-          </div>
-        </main>
-      </article>
-      // </LoadingScreen>
+          </main>
+        </article>
+      </LoadingScreen>
     );
   }
 }
